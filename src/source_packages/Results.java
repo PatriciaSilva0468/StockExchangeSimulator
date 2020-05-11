d0a31312b69a84b0910f4f58e068b0c6d25a7916
Patricia <patriciasilva.mural@gmail.com>
2020-05-12 00:21:49 +0100

    Creating interface
    Interface is called Results

---
+++ src/source_packages/Results.java
@@ -0,0 +1,9 @@
+package source_packages;
+
+public interface Results {
+    void printMaxCapitalCompany();
+    void printMinCapitalCompany();
+    void printMinSharesInvestor();
+    void printMaxSharesInvestor();
+    void printTheMenu();
+}
