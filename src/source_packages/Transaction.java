e816de49684d17efb6beff2012fe9f5ad8119ca0
Patricia <patriciasilva.mural@gmail.com>
2020-05-12 00:28:46 +0100

    Creating class Transaction
    This class has the investorID, sharePrice and companyID

---
+++ src/source_packages/Transaction.java
@@ -0,0 +1,31 @@
+package source_packages;
+
+public class Transaction {
+    private int investorID;
+    private double sharePrice;
+    private int companyID;
+
+    public int getInvestorID() {
+        return investorID;
+    }
+
+    public void setInvestorID(int investorID) {
+        this.investorID = investorID;
+    }
+
+    public double getSharePrice() {
+        return sharePrice;
+    }
+
+    public void setSharePrice(double sharePrice) {
+        this.sharePrice = sharePrice;
+    }
+
+    public int getCompanyID() {
+        return companyID;
+    }
+
+    public void setCompanyID(int companyID) {
+        this.companyID = companyID;
+    }
+}
