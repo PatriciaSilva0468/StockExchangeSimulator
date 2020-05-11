882c6af6236ef7786890fa807afead63754544e0
Patricia <patriciasilva.mural@gmail.com>
2020-05-12 00:41:55 +0100

    Adding data type double for companyCapital
    Also adding comments to companyCapital class

--- src/source_packages/Company.java
+++ src/source_packages/Company.java
@@ -1,6 +1,11 @@
+/**
+ * This class represents Company.
+ * It has an id, number of shares (which might be updated later),
+ * original shares (cannot be updated),
+ * share price
+ * and number of shared sold
+ */
 package source_packages;
-
-import java.util.ArrayList;
 
 public class Company {
     private int id;
@@ -9,6 +14,7 @@ public class Company {
     private int totalNumberOfShares;
     private double sharePrice;
     private int sharesSold;
+    private double companyCapital;
 
     public int getOriginalShares() {
         return originalShares;
@@ -46,12 +52,27 @@ public class Company {
         return sharesSold;
     }
 
+    public double getCompanyCapital() {
+        return companyCapital;
+    }
+
+    public void setCompanyCapital(double companyCapital) {
+        this.companyCapital = companyCapital;
+    }
+
+    /*
+     * Many investment threads will be running parallel and there is a chance
+     * that more than one thread accesses a company at same time. So to prevent problems
+     * caused by parallel calling to this method it is made synchronized
+     */
     public synchronized void incrementSharesSold() {
         ++sharesSold;
         --numberOfShares;
     }
 
 
+    /*
+    * On each selling of 10 shares, the share price doubles up*/
     public boolean checkIfTenSharesSold() {
         if (sharesSold >= 10 && sharesSold % 10 == 0) {
             // double share price
@@ -64,12 +85,14 @@ public class Company {
 
     }
 
+    /*
+    * To string method representing the object in appropriate string form*/
     @Override
     public String toString() {
         return "Company ID: " + this.id +
                 "\nCompany shares: " + this.numberOfShares +
                 "\nCompany share price: " + this.sharePrice +
                 "\nCompany share sold: " + this.sharesSold +
-                "\nCompany capital: " + (this.sharePrice * this.numberOfShares);
+                "\nCompany capital: " + this.getCompanyCapital();
     }
 }
