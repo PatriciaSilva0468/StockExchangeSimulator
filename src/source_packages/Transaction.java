112ff4be2bdde4416c8261910cbb68efbbf84270
Patricia <patriciasilva.mural@gmail.com>
2020-05-12 01:03:18 +0100

    Commeting Transaction class

--- src/source_packages/Transaction.java
+++ src/source_packages/Transaction.java
@@ -1,9 +1,18 @@
+/**
+ * This class represents Transactions made by the investor.
+ * It contains the ID of investor,
+ * ID of company in which an investment is made
+ * and share price of the share bought
+ */
 package source_packages;
-
 public class Transaction {
     private int investorID;
     private double sharePrice;
     private int companyID;
+
+    /*
+     * Here comes getters and setters of the attributes
+     * */
 
     public int getInvestorID() {
         return investorID;
